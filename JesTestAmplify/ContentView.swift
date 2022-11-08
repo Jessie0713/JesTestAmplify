a8a56cf22a2cf7bb5f337f42ea900a3f86b5acfd
許湘潔 <jessiehsu@candyhouse.co>
2022-11-08 14:54:10 +0800

    Initial Commit

---
+++ JesTestAmplify/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  JesTestAmplify
+//
+//  Created by 許湘潔 on 2022/11/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JesTestAmplify/JesTestAmplifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JesTestAmplifyApp.swift
+//  JesTestAmplify
+//
+//  Created by 許湘潔 on 2022/11/8.
+//
+
+import SwiftUI
+
+@main
+struct JesTestAmplifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
